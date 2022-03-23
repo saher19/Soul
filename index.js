bbd8d4073363d4ee99037dd55032d90db36b2b2e
saher19 <84818638+saher19@users.noreply.github.com>
2022-03-23 15:35:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,9 @@
-PS3="Please select your choice: "
-CHOICE=("Ubuntu" "Debian" "Raspbian" "CentOS" "Exit")
-select CHC in "${CHOICE[@]}"
-do
-	case $CHC in
-		"Ubuntu")
-				#define distribution
-				DIST="ubuntu"
-
-				#make scripts directory
-				mkdir -v $HOME/scripts
-
-				wget $SCRIPT_DL_URL/$DIST/$DIST-install.sh -P $HOME
-				source $HOME/$DIST-install.sh
-				rm -f -v $HOME/$DIST-install.sh
-				rm -f -v $HOME/linux-install.sh
-
-				#do we want to reboot the system
-				read -r -p "All done! Do you want to reboot? (Y/N) " ANSWER
-				echo
-				if [[ $ANSWER =~ ^([yY])$ ]]
-				then
-					shutdown -r 1 Press CTRL+C to abort.
-				fi
-
-				#we are done. exit the script
-				exit
-			;;
-		"Debian")
-				#define distribution
-				DIST="debian"
-
-				#make scripts directory
-				mkdir -v $HOME/scripts
-
-				wget $SCRIPT_DL_URL/$DIST/$DIST-install.sh -P $HOME
-				source $HOME/$DIST-install.sh
-				rm -f -v $HOME/$DIST-install.sh
-				rm -f -v $HOME/linux-install.sh
-
-				#do we want to reboot the system
-				read -r -p "All done! Do you want to reboot? (Y/N) " ANSWER
-				echo
-				if [[ $ANSWER =~ ^([yY])$ ]]
-				then
-					shutdown -r 1 Press CTRL+C to abort.
-				fi
-
-				#we are done. exit the script
-				exit
-			;;
-		"Raspbian")
+module.exports = {
+	nameMapping: {
+		FsStats: /^Stats Import fs/,
+		validateFunction: /^validate Import/,
+		Configuration: /^WebpackOptions /
+	},
+	exclude: [/^devServer in WebpackOptions /],
+	include: [/^(_module|_compilation|_compiler) in NormalModuleLoaderContext /]
+};
